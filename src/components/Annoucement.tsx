4c4c96e5841ff06d09bf7d3aee52b471ede05050
sidialicoulibaly <coulibalysidiali06@gmail.com>
2025-01-08 12:38:44 +0000

    ajout de l'espace dans annonce

--- src/components/Annoucement.tsx
+++ src/components/Annoucement.tsx
@@ -27,7 +27,7 @@ const Announcement = async () => {
   return (
     <div className="bg-white p-4 rounded-md">
       <div className="flex items-center justify-between">
-        <h1 className="text-xl font-semibold">Annonces</h1>
+        <h1 className="text-xl font-semibold">Annonces </h1>
         <span className="text-xs text-gray-400">Voir plus</span>
       </div>
       <div className="flex flex-col gap-4 mt-4">
